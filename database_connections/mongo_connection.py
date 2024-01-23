5d8dec6c4bd7fce9cfb06ebb4fea0c318bcf4158
ivanica9598 <ivanapmilivojevic@gmail.com>
2024-01-23 19:33:02 +0100

    add local db connection string

--- database_connections/mongo_connection.py
+++ database_connections/mongo_connection.py
@@ -6,8 +6,10 @@ from config import conf
 
 def mongo_db_connect():
     try:
-        con = connect(host=conf["MONGODB_CONNECTION"] + conf["MONGODB_NAME"])
-        db = con.get_database(conf["MONGODB_NAME"])
+        # con = connect(host=conf["MONGODB_CONNECTION"] + conf["MONGODB_NAME"])
+        # db = con.get_database(conf["MONGODB_NAME"])
+        con = connect(host=conf["MONGODB_CONNECTION_LOCAL"] + conf["MONGODB_NAME_LOCAL"])
+        db = con.get_database(conf["MONGODB_NAME_LOCAL"])
 
         print("Mongo: Connecting successful")
 
